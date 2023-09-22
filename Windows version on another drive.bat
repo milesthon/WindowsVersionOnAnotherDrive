41f41c9634c371399e592138b8d6e44cfe81b916
Michael Milesthon <103425609+milesthon@users.noreply.github.com>
2023-09-22 13:48:54 +0700

    Add files via upload

--- Windows version on another drive.bat
+++ Windows version on another drive.bat
@@ -10,19 +10,31 @@ if "%disk%" == "c" goto CDrive
 ECHO.&ECHO REG LOAD..&ECHO.
 REG UNLOAD HKLM\%disk%                                        2>nul >nul
 REG LOAD HKLM\%disk% %disk%:\Windows\System32\config\SOFTWARE 2>nul >nul
+REG LOAD HKLM\%disk%2 %disk%:\Windows\System32\config\SYSTEM  2>nul >nul
 cls
-FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%\Microsoft\Windows NT\CurrentVersion" /v ProductName')  Do set "ProductName=%%I %%J"
-FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%\Microsoft\Windows NT\CurrentVersion" /v EditionID')    Do set "EditionID=%%I %%J"
-FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild') Do set "CurrentBuild=%%I %%J"
+FOR /F "tokens=4,5" %%I in ('reg query "HKLM\%disk%2\CurrentControlSet\Services\Tcpip\Parameters" /v "NV Hostname"')                    Do set "Hostname=%%I %%J"
+FOR /F "tokens=4,5" %%I in ('reg query "HKLM\%disk%2\CurrentControlSet\Services\Tcpip\Parameters" /v "NV Domain"')                      Do set "Domain=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%\Microsoft\Windows NT\CurrentVersion" /v ProductName')                               Do set "ProductName=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%\Microsoft\Windows NT\CurrentVersion" /v EditionID')                                 Do set "EditionID=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild')                              Do set "CurrentBuild=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\%disk%2\CurrentControlSet\Control\Session Manager\Environment" /v Processor_architecture') Do if [%%I]==[AMD64] ( set "architecture=x64" ) ELSE ( set "architecture=x32" )
 ECHO.&ECHO REG UNLOAD..&ECHO.
-REG UNLOAD HKLM\%disk%                                        2>nul >nul
+REG UNLOAD HKLM\%disk%  2>nul >nul
+REG UNLOAD HKLM\%disk%2 2>nul >nul
 cls
-ECHO.&ECHO %ProductName% %EditionID%%CurrentBuild%&ECHO.
+ECHO.&ECHO HOSTNAME: %Hostname% 
+ECHO DOMAIN: %Domain%
+ECHO OS: %ProductName% %EditionID%%CurrentBuild%%architecture% &ECHO.
 PAUSE&EXIT
 
 :CDrive
-FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName')  Do set "ProductName=%%I %%J"
-FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v EditionID')    Do set "EditionID=%%I %%J"
-FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild') Do set "CurrentBuild=%%I %%J"
-ECHO.&ECHO %ProductName% %EditionID%%CurrentBuild%&ECHO.
+FOR /F "tokens=4,5" %%I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "NV Hostname"')                    Do set "Hostname=%%I %%J"
+FOR /F "tokens=4,5" %%I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "NV Domain"')                      Do set "Domain=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName')                            Do set "ProductName=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v EditionID')                              Do set "EditionID=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild')                           Do set "CurrentBuild=%%I %%J"
+FOR /F "tokens=3,4" %%I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Processor_architecture') Do if [%%I]==[AMD64] ( set "architecture=x64" ) ELSE ( set "architecture=x32" )
+ECHO.&ECHO HOSTNAME: %Hostname% 
+ECHO DOMAIN: %Domain%
+ECHO OS: %ProductName% %EditionID%%CurrentBuild%%architecture% &ECHO.
 PAUSE&EXIT
